7cfeaa3e642bfb82899de766aada201d003647aa
Guy Korland <gkorland@gmail.com>
2023-09-10 14:07:02 +0300

    blue

--- app/components/navigation/navbar/Logo.tsx
+++ app/components/navigation/navbar/Logo.tsx
@@ -39,8 +39,8 @@ const Logo = () => {
         <Image
           src="/images/logo.png"
           alt="Logo"
-          width={width < 1024 ? "150" : "250"}
-          height={width < 1024 ? "45" : "74"}
+          width={width < 1024 ? "120" : "150"}
+          height={width < 1024 ? "30" : "74"}
           className="relative"
         />
       </Link>
--- app/components/navigation/navbar/index.tsx
+++ app/components/navigation/navbar/index.tsx
@@ -6,7 +6,7 @@ import Button from "./Button";
 const Navbar = ({ toggle }: { toggle: () => void }) => {
   return (
     <>
-      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+      <div className="w-full h-20 bg-blue-600 sticky top-0">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
             <Logo />
