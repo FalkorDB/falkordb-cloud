fdaed1266f7e5d890508ece9dc95261c616030e0
Guy Korland <gkorland@gmail.com>
2023-09-10 14:23:19 +0300

    fix build issue

--- app/components/SignInButtons.tsx
+++ app/components/SignInButtons.tsx
@@ -3,21 +3,27 @@
 import { signIn } from "next-auth/react"
 import { getProviders } from "next-auth/react";
 
-export default async function SignInButtons() : Promise<any> {
+export default async function SignInButtons(): Promise<any> {
 
-  const providers = await getProviders();
+    const providers = await getProviders();
 
-  return (
-    <div>
-      {Object.values(providers).map((provider) => {
-        return (
-          <div key={provider.name}>
-            <button onClick={() => signIn(provider.id)}>
-              Sign in with {provider.name}
-            </button>
-          </div>
-        );
-      })}
-    </div>
-  );
+    // Check if providers is null
+    if (!providers) {
+        // Display an error message or a loading indicator
+        return <div>Unable to load providers</div>;
+    }
+
+    return (
+        <div>
+            {Object.values(providers).map((provider) => {
+                return (
+                    <div key={provider.name}>
+                        <button onClick={() => signIn(provider.id)}>
+                            Sign in with {provider.name}
+                        </button>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
