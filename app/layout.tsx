542051d315e827987fb4967422465e1a43bdbf0a
Guy Korland <gkorland@gmail.com>
2023-09-12 23:02:50 +0300

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { NextAuthProvider } from "./providers";
+import GoogleAnalytics from "@/app/components/GoogleAnalytics";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+          <GoogleAnalytics ga_id=
+            {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+        ) : null}
         <NextAuthProvider>{children}</NextAuthProvider>
       </body>
     </html>
