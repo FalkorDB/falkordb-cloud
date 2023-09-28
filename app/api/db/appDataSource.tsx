15c3f503fb94f2c364aaf70ba04ec41e47909869
Guy Korland <gkorland@gmail.com>
2023-09-28 12:14:00 +0300

    get public IP

--- app/api/db/appDataSource.tsx
+++ app/api/db/appDataSource.tsx
@@ -2,13 +2,15 @@ import { DataSource, EntitySchema } from "typeorm";
 import dataSourceOptions from "./options";
 import { AccountEntity, SessionEntity, UserEntity } from "../models/entities";
 
-export const AppDataSource = new DataSource({
+const dataSource = new DataSource({
     ...dataSourceOptions,
     entities: [UserEntity, AccountEntity, SessionEntity, EntitySchema]
 })
 
-AppDataSource.initialize()
+dataSource.initialize()
     .then(() => {
         // here you can start to work with your database
     })
     .catch((error) => console.log(error))
+
+export default dataSource;
