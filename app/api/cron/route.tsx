5850cc40ff37fa6e1b46489736b5545176e8e9a0
Guy Korland <gkorland@gmail.com>
2023-10-02 20:19:47 +0300

    remove printouts

--- app/api/cron/route.tsx
+++ app/api/cron/route.tsx
@@ -3,14 +3,8 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
 
-
   // check if Authorization header is contains cron token
   let authorization = request.headers.get('Authorization');
-
-  console.warn(authorization)
-  console.warn(process.env.CRON_SECRET)
-
-
   if (!authorization || authorization !== process.env.CRON_SECRET) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
   } 
