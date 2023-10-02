497048ca642049087a95b56f2ba7221aaa4f0955
Guy Korland <gkorland@gmail.com>
2023-10-02 20:00:40 +0300

    check CRON Authorization

---
+++ app/api/cron/route.tsx
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server';
+
+
+export async function GET(request: Request) {
+
+
+  // check if Authorization header is contains cron token
+  let authorization = request.headers.get('Authorization');
+
+  console.warn(authorization)
+  console.warn(process.env.CRON_TOKEN)
+
+
+  if (!authorization || authorization !== process.env.CRON_TOKEN) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+  } 
+
+  return NextResponse.json({ ok: true });
+
+}
