bc35e762cbf90ed59f91f398c21bdfd9f7c1fa6d
Guy Korland <gkorland@gmail.com>
2023-10-05 13:35:43 +0300

    center

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <main className="flex flex-col items-center flex-1 px-20 text-center">
+      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
         <div className="space-y-8">
           <div className="space-y-2">
             <h2 className="text-3xl font-bold">Contact Us</h2>
