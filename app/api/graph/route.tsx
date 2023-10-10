e074010838084f73e40f9cafcaabf5700459981d
AviAvni <avi.avni@gmail.com>
2023-10-10 15:43:51 +0300

    add ability to load sample database

--- app/api/graph/route.tsx
+++ app/api/graph/route.tsx
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth/next"
 import { NextRequest, NextResponse } from "next/server";
 import { UserEntity } from '../models/entities';
 import { createClient } from 'redis';
+import fs from 'fs/promises';
 
 export async function GET() {
 
@@ -22,14 +23,52 @@ export async function GET() {
         email: email
     })
 
-    const client = await createClient( {
+    const client = await createClient({
         url: `redis://:${user?.db_password}@${user?.db_host}:${user?.db_port}`
     }).connect();
 
-    try{
+
+    try {
         let result = await client.graph.list()
-        return NextResponse.json({ result: result }, { status: 200 })
+        let files = (await fs.readdir('public/examples')).map(f => f.split('.')[0]);
+        return NextResponse.json({ result: { graphs: result, examples: files } }, { status: 200 })
     } catch (err: any) {
         return NextResponse.json({ message: err.message }, { status: 400 })
     }
+}
+
+export async function POST(req: NextRequest) {
+    const session = await getServerSession(authOptions)
+
+    if (!session) {
+        return NextResponse.json({ message: "You must be logged in." }, { status: 401 })
+    }
+
+    const email = session.user?.email;
+    if (!email) {
+        return NextResponse.json({ message: "Can't find user details" }, { status: 500 })
+    }
+
+    let manager = await getEntityManager()
+    const user = await manager.findOneBy(UserEntity, {
+        email: email
+    })
+
+    const client = await createClient({
+        url: `redis://:${user?.db_password}@${user?.db_host}:${user?.db_port}`
+    }).connect();
+
+    let body = await req.json()
+    let name = body.name
+    console.log(name)
+
+    try {
+        const data = await fs.readFile(`public/examples/${name}.dump`, { encoding: 'hex' });
+        const buffer = Buffer.from(data, 'hex');
+        await client.restore(name, 0, buffer, { REPLACE: true });
+    } catch (err) {
+        return NextResponse.json({ message: err }, { status: 400 })
+    }
+
+    return NextResponse.json({ result: name }, { status: 200 })
 }
--- app/sandbox/GraphsList.tsx
+++ app/sandbox/GraphsList.tsx
@@ -1,12 +1,14 @@
 import { useState, useEffect, Dispatch, SetStateAction } from 'react';
 import { Combobox } from '../components/combobox';
 import { useToast } from "@/components/ui/use-toast"
+import { Button } from '@/components/ui/button';
 
 
 // A component that renders an input box for Cypher queries
 export function GraphsList(props: { onSelectedGraph: Dispatch<SetStateAction<string>> }) {
 
     const [graphs, setGraphs] = useState<string[]>([]);
+    const [examples, setExamples] = useState<string[]>([]);
     const [selectedGraph, setSelectedGraph] = useState("");
     const { toast } = useToast()
 
@@ -27,7 +29,8 @@ export function GraphsList(props: { onSelectedGraph: Dispatch<SetStateAction<str
                 })
                 return { result: [] }
             }).then((result) => {
-                setGraphs(result.result)
+                setGraphs(result.result.graphs)
+                setExamples(result.result.examples)
             })
     }, [toast])
 
@@ -41,7 +44,38 @@ export function GraphsList(props: { onSelectedGraph: Dispatch<SetStateAction<str
         props.onSelectedGraph(graph)
     }
 
+    function addSampleDatabase(sample: string) {
+        fetch('/api/graph', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name: sample })
+        })
+            .then((result) => {
+                if (result.status < 300) {
+                    return result.json()
+                }
+                toast({
+                    title: "Error",
+                    description: result.text(),
+                })
+                return { result: [] }
+            }).then((result) => {
+                graphs.push(sample)
+                setGraphs(graphs)
+                setSelectedGraph(sample)
+            })
+    }
+
+    let samples_list = examples.map((sample) => {
+        return <Button className="rounded-full bg-blue-600 p-2 text-slate-50 m-2" onClick={ev => addSampleDatabase(sample)}>{sample}</Button>
+    })
+
     return (
-        <Combobox type={"Graph"} options={graphs} addOption={addOption} selectedValue={selectedGraph} setSelectedValue={setSelectedValue} />
+        <div className='flex-row'>
+            {samples_list}
+            <Combobox type={"Graph"} options={graphs} addOption={addOption} selectedValue={selectedGraph} setSelectedValue={setSelectedValue} />
+        </div>
     );
 }
