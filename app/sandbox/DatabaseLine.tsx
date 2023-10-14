eabb1331ccb5757ec97249ef0c57cb2ca2b855d9
Guy Korland <gkorland@gmail.com>
2023-10-14 15:11:00 +0300

    fix C&P

--- app/sandbox/DatabaseLine.tsx
+++ app/sandbox/DatabaseLine.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label";
 import { useToast } from "@/components/ui/use-toast"
-import { Copy, Eye } from "lucide-react";
+import { Copy, Eye, EyeOff } from "lucide-react";
 import { useState } from "react";
 
 export function DatabaseLine(props: { label: string, value: string, masked?: string }) {
@@ -9,8 +9,8 @@ export function DatabaseLine(props: { label: string, value: string, masked?: str
     const { toast } = useToast()
     const [showPassword, setShowPassword] = useState(false);
 
-    function copyToClipboard(event: any) {
-        navigator.clipboard.writeText(event.target.getAttribute("aria-label"))
+    function copyToClipboard(value: string) {
+        navigator.clipboard.writeText(value)
         toast({
             title: "Copied to clipboard",
             description: "The value has been copied to your clipboard.",
@@ -23,14 +23,14 @@ export function DatabaseLine(props: { label: string, value: string, masked?: str
 
     return (
         <div className="flex flex-row space-x-0">
-            <div className="py-2">{props.label}:</div>            
-            <Button className="bg-transparent text-blue-600 px-2 hover:text-slate-50" onClick={copyToClipboard} aria-label={props.value.toString()}>
+            <div className="py-2">{props.label}:</div>
+            <Button className="bg-transparent text-blue-600 px-2 hover:text-slate-50" onClick={() => copyToClipboard(props.value)}>
                 <div>{(showPassword || !props.masked) ? props.value : props.masked}</div>
-                <Copy className="ml-2 px-0"/>
+                <Copy className="ml-2 px-0" />
             </Button>
             {props.masked &&
                 <Button className="bg-transparent text-blue-600 px-2 hover:text-slate-50" onClick={showMasked}>
-                    <Eye  className="m-0 p-0"/>
+                    {showPassword ? <EyeOff className="m-0 p-0" /> : <Eye className="m-0 p-0" />}
                 </Button>
             }
         </div>
