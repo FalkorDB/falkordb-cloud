0e23756eb4f36d772ac8ac294c91224e4bb81ab6
Guy Korland <gkorland@gmail.com>
2023-10-19 16:05:12 +0300

    add remove ca.crt file

--- app/sandbox/DatabaseDetails.tsx
+++ app/sandbox/DatabaseDetails.tsx
@@ -2,21 +2,31 @@ import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, Di
 import { Button } from "@/components/ui/button"
 import { Sandbox } from "@/app/api/db/sandbox";
 import { DatabaseLine } from "./DatabaseLine";
+import Link from "next/link";
 
 export function DatabaseDetails(props: { sandbox: Sandbox, onDelete: () => void }) {
 
     let sandbox = props.sandbox
     let redisURL = `rediss://${sandbox.password}@${sandbox.host}:${sandbox.port}`
     let redisURLMasked = `rediss://********@${sandbox.host}:${sandbox.port}`
+
+    
+    let caURL = null;
+    if(sandbox.tls) {
+        const blob = new Blob([sandbox.cacert], {type: 'text/plain'});
+        caURL = URL.createObjectURL(blob);
+    }
+
+    
     return (
         <>
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap items-center space-x-2">
                 <DatabaseLine label="Host" value={sandbox.host} />
                 <DatabaseLine label="Port" value={sandbox.port.toString()} />
                 <DatabaseLine label="Password" value={sandbox.password} masked="********" />
                 <DatabaseLine label="Redis URL" value={redisURL} masked={redisURLMasked} />
-                {sandbox.tls &&
-                    <DatabaseLine label="CaCert" value={sandbox.cacert} masked="***" />
+                {caURL &&
+                    <a download="ca.crt" href={caURL}><u>CA certificate</u></a>
                 }
                 <Dialog>
                     <DialogTrigger>
