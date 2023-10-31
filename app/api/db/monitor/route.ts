78e24f31f0871e751107aa333e6eb08a88242e5f
AviAvni <avi.avni@gmail.com>
2023-10-31 16:29:26 +0200

    fix get metrics

--- app/api/db/monitor/route.ts
+++ app/api/db/monitor/route.ts
@@ -43,36 +43,6 @@ const displayMetricData = (metricName: string, dataPoints: any[]) => {
 async function monitorECSTask(region: Region, user: UserEntity, serviceArn: string) {
   try {
 
-    let waitECSTask = await waitUntilServicesStable(
-      { client: region.ecsClient, maxWaitTime: 5, minDelay: 1 },
-      { cluster: "falkordb", services: [serviceArn] }
-    )
-
-    // Task is not ready yet
-    if (waitECSTask.state != 'SUCCESS') {
-      return;
-    }
-
-    const tasks = await region.ecsClient.send(new ListTasksCommand({
-      cluster: "falkordb",
-      serviceName: `falkordb-${user.id}`,
-    }))
-
-    // Get the task details from ECS
-    const command = new DescribeTasksCommand({ cluster: region.clusterARN, tasks: tasks.taskArns });
-    const response = await region.ecsClient.send(command);
-    const taskDetails = response.tasks?.[0];
-    if (!taskDetails) {
-      throw new Error('Task not found');
-    }
-
-    // Get the container instance ID and the container ID from the task details
-    const containerInstance = taskDetails.containerInstanceArn;
-    const container = taskDetails.containers?.[0].containerArn;
-    if (!containerInstance || !container) {
-      throw new Error('Container instance or container not found');
-    }
-
     // Define the time range and the period for the metrics
     const endTime = new Date();
     const startTime = new Date(endTime.getTime() - 3600 * 1000); // One hour ago
@@ -82,7 +52,7 @@ async function monitorECSTask(region: Region, user: UserEntity, serviceArn: stri
     const cpuUtilizationData = await getMetricStatistics(region.cwClient,
       'AWS/ECS',
       'CPUUtilization',
-      [{ Name: 'ClusterName', Value: 'falkordb' }, { Name: 'ContainerInstance', Value: containerInstance }],
+      [{ Name: 'ClusterName', Value: 'falkordb' }, { Name: 'ServiceName', Value: `falkordb-${user.id}` }],
       startTime,
       endTime,
       period,
@@ -93,7 +63,7 @@ async function monitorECSTask(region: Region, user: UserEntity, serviceArn: stri
     const memoryUtilizationData = await getMetricStatistics(region.cwClient,
       'AWS/ECS',
       'MemoryUtilization',
-      [{ Name: 'ClusterName', Value: 'falkordb' }, { Name: 'ContainerInstance', Value: containerInstance }],
+      [{ Name: 'ClusterName', Value: 'falkordb' }, { Name: 'ServiceName', Value: `falkordb-${user.id}` }],
       startTime,
       endTime,
       period,
@@ -101,40 +71,37 @@ async function monitorECSTask(region: Region, user: UserEntity, serviceArn: stri
     );
 
     // Get the network in metric for the container
-    const networkInData = await getMetricStatistics(region.cwClient,
-      'AWS/ECS',
-      'NetworkRxBytes',
-      [{ Name: 'ClusterName', Value: 'falkordb' }, { Name: 'TaskId', Value: tasks.taskArns }, { Name: 'ContainerName', Value: container }],
-      startTime,
-      endTime,
-      period,
-      ['Average']
-    );
+    // const networkInData = await getMetricStatistics(region.cwClient,
+    //   'AWS/ECS',
+    //   'NetworkRxBytes',
+    //   [{ Name: 'ClusterName', Value: 'falkordb' }, { Name: 'TaskId', Value: tasks.taskArns }, { Name: 'ContainerName', Value: container }],
+    //   startTime,
+    //   endTime,
+    //   period,
+    //   ['Average']
+    // );
 
     // Get the network out metric for the container
-    const networkOutData = await getMetricStatistics(region.cwClient,
-      'AWS/ECS',
-      'NetworkTxBytes',
-      [{ Name: 'ClusterName', Value: 'falkordb' }, { Name: 'TaskId', Value: tasks.taskArns }, { Name: 'ContainerName', Value: container }],
-      startTime,
-      endTime,
-      period,
-      ['Average']
-    );
+    // const networkOutData = await getMetricStatistics(region.cwClient,
+    //   'AWS/ECS',
+    //   'NetworkTxBytes',
+    //   [{ Name: 'ClusterName', Value: 'falkordb' }, { Name: 'TaskId', Value: tasks.taskArns }, { Name: 'ContainerName', Value: container }],
+    //   startTime,
+    //   endTime,
+    //   period,
+    //   ['Average']
+    // );
 
     // Display the metric data in the console
     displayMetricData('CPU Utilization (%)', cpuUtilizationData.Datapoints ?? []);
     displayMetricData('Memory Utilization (%)', memoryUtilizationData.Datapoints ?? []);
-    displayMetricData('Network In (Bytes)', networkInData.Datapoints ?? []);
-    displayMetricData('Network Out (Bytes)', networkOutData.Datapoints ?? []);
+    // displayMetricData('Network In (Bytes)', networkInData.Datapoints ?? []);
+    // displayMetricData('Network Out (Bytes)', networkOutData.Datapoints ?? []);
+    return { cpuUtilizationData, memoryUtilizationData };
   } catch (error) {
     console.error(error);
   }
 };
-
-
-const data = [34, 30, 24, 92, 35, 47, 60]
-const xindex = [10, 20, 30, 40, 50, 60, 70]
 
 export async function GET() {
 
@@ -153,7 +120,7 @@ export async function GET() {
     return NextResponse.json({ message: `Task Get failed, can't find region: ${regionName}` }, { status: 500 })
   }
 
-  // monitorECSTask(region, user, user.task_arn)
+  monitorECSTask(region, user, user.task_arn)
 
 
 
