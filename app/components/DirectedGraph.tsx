911a2c3a9348798240eb6dabc88a0185b6ef198d
Guy Korland <gkorland@gmail.com>
2023-11-22 16:57:35 +0200

    Staging (#161)
    * edit signin page
    
    * add error message on login error
    
    * add node click to expend

--- app/components/DirectedGraph.tsx
+++ app/components/DirectedGraph.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactEcharts, { EChartsOption } from "echarts-for-react";
+import React, { useRef } from 'react';
+import ReactEcharts, { EChartsInstance } from "echarts-for-react";
 
 export interface Category {
   name: string,
@@ -41,17 +41,23 @@ function getOption(nodes: GraphData[], edges: GraphLink[], categories: Category[
     },
     series: [
       {
+        nodes,
+        edges,
+        categories,
+
         type: "graph",
         layout: "force",
+        force: {
+          edgeLength: 70,
+          repulsion: 150,
+          gravity: 0.1
+        },
         draggable: true,
         label: {
           position: 'center',
           show: true,
-          formatter: '{b}',         
+          formatter: '{b}',
         },
-        nodes: nodes,
-        edges: edges,
-        categories: categories,
         emphasis: {
           focus: 'adjacency',
           label: {
@@ -61,14 +67,12 @@ function getOption(nodes: GraphData[], edges: GraphLink[], categories: Category[
         },
         roam: true,
         lineStyle: {
+          color: 'source',
           width: 3.0,
-          curveness: 0.2,
+          curveness: 0.1,
+          opacity: 0.7
         },
-        dagreLayout: {
-          rankdir: 'LR', // Left to right layout
-          nodesepFunc: () => 1, // Node separation function
-          ranksepFunc: () => 1, // Rank separation function
-        },
+        symbolSize: 30,
       },
     ],
   };
@@ -76,26 +80,86 @@ function getOption(nodes: GraphData[], edges: GraphLink[], categories: Category[
 
 export function DirectedGraph(
   props: {
-    nodes: GraphData[],
-    edges: GraphLink[],
-    categories: Category[],
-    onChartClick: (id: number) => Promise<[Category[], GraphData[], GraphLink[]]>
+    nodes: Map<number, GraphData>,
+    edges: Set<GraphLink>,
+    categories: Map<String, Category>,
+    onChartClick: (id: number) => Promise<[Map<String, Category>, Map<number, GraphData>, Set<GraphLink>]>
   }) {
+  const echartRef = useRef<EChartsInstance | null>(null)
 
-  // let [nodes, setNodes] = React.useState<GraphData[]>(props.data)
-  // let [edges, setEdges] = React.useState<GraphLink[]>(props.links)
-  // let [categories, setCategories] = React.useState<Category[]>(props.categories)
+  const nodes = Array.from(props.nodes.values())
+  const edges = Array.from(props.edges.values())
+  const categories = Array.from(props.categories.values())
 
-  // let onEvents: { echartRef: any, click: (params: any) => void } = {
-  //   echartRef: null,
-  //   click: async (params: any) => {
-  //     let [newCategories, newNodes, newEdges] = await props.onChartClick(parseInt(params.data.id))
-      
-  //     setNodes([...nodes, ...newNodes]);
-  //     setEdges([...edges, ...newEdges]);
-  //   }
-  // }
+  let options = getOption(nodes, edges, categories)
 
-  // return (<ReactEcharts ref={(e) => { onEvents.echartRef = e }} option={getOption(nodes, edges, categories)} onEvents={onEvents} />)
-  return (<ReactEcharts className="border" option={getOption(props.nodes, props.edges, props.categories)} />)
+  let onEvents: { click: (params: any) => void } = {
+    click: async (params: any) => {
+      let [newCategories, newNodes, newEdges] = await props.onChartClick(parseInt(params.data.id))
+
+      let newCategoriesArray = new Array<Category>(newCategories.size)
+      newCategories.forEach((category) => {
+        newCategoriesArray[category.index] = category
+      })
+
+      newNodes.forEach((node, id) => {
+        if (!props.nodes.get(id)) {
+
+          // Check if category already exists in categories otherwise add it
+          let newCategory = newCategoriesArray[node.category]
+          let category = props.categories.get(newCategory.name)
+
+          if(category) {
+            node.category = category.index
+          } else {
+            newCategory.index = categories.length
+            node.category = newCategory.index
+            props.categories.set(newCategory.name, newCategory)
+            categories.push(newCategory)
+          }
+
+          // Add the node to the nodes map and array
+          props.nodes.set(parseInt(node.id), node)
+          nodes.push(node)
+        }
+      })
+
+      newEdges.forEach((edge) => {
+        if (!props.edges.has(edge)) {
+          props.edges.add(edge)
+          edges.push(edge)
+        }
+      })
+
+      echartRef.current?.setOption({
+        legend: [
+          {
+            data: categories.map(function (c) {
+              return c.name;
+            })
+          }
+        ],
+        series: [
+          {
+            nodes,
+            edges,
+            categories,
+          }
+
+        ]
+      })
+    }
+  }
+
+  return (
+    <ReactEcharts
+      style={{ height: "50vh"}}
+      className="border"
+      option={options}
+      onEvents={onEvents}
+      onChartReady={
+        (e) => { echartRef.current = e }
+      }
+    />
+  )
 }
